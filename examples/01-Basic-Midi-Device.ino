6a0c94c63eb7056aa69d923cd6c306e5143ff5b9
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2020-10-09 22:38:36 +0200

    Typos

--- examples/01-Basic-Midi-Device.ino
+++ examples/01-Basic-Midi-Device.ino
@@ -12,7 +12,7 @@ void setup() {
 }
 
 void loop() {
-  if(bleMidi.isConnected()) {             // If we've got a connection, we send an A4 during one second, at full elocity (127)
+  if(bleMidi.isConnected()) {             // If we've got a connection, we send an A4 during one second, at full velocity (127)
       bleMidi.noteOn(0, 69, 127);
       delay(1000);
       bleMidi.noteOff(0, 69, 127);        // Then we make a delay of one second before returning to the beginning of the loop
